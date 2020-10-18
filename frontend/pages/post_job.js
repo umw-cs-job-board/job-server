7bea23d0c7c8dfd3023282fd31ce2e35827bdd91
Waxillium <rkauffma@mail.umw.edu>
2020-10-18 17:07:34 -0400

    set up for front end development

--- frontend/pages/post_job.js
+++ frontend/pages/post_job.js
@@ -1,1 +1,22 @@
 import React from 'react'
+
+class PostJob extends React.Component{
+
+	render(){
+		return(
+			<div>
+				<h1>Post Job</h1>
+
+				<style jsx>{`
+					div{
+						text-align: center;
+						font-family: Arial;
+					}
+				`}</style>
+			</div>
+		);
+	}
+
+}
+
+export default PostJob;
