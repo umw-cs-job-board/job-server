22221c6c46ca90c1835f7307fe09213107c09f42
milesspence1 <milesspence1@gmail.com>
2020-10-18 23:22:20 +0000

    Changed lines 30 and 31, added some comments.

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,6 @@ app.set("port", 8080);
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-//new user: jbuser, password: $3Lxq#r37
-//Login using: psql -h localhost -U jbuser jobboard
 const Pool = require("pg").Pool;
 const config = {
 	host: "localhost",
@@ -30,52 +27,30 @@ app.get("/", async (req, res) => {
 		}
 });
 
-//create job and add it to the database
-app.post("/create-job", async (req, res) => {
-
-	console.log("----------");
-
+//Remove Job
+app.post("/remove-job", async (req, res) =>{
+	const id = req.body.id;
 	try {
+		//Creating a query to check if the job to be removed exists in the job database.
+		const template1 = "SELECT title FROM jobs WHERE id = $1";
+		const response1 = await pool.query(template1, id);
 		
-		console.log(req.body);
-		//title, employer_name, location, start_date , end_date, description
-		const title_to_add = req.body.title;
-		const employer_name_to_add = req.body.employer_name;
-		const location_to_add = req.body.location;
-		const start_date_to_add = req.body.start_date;
-		const end_date_to_add = req.body.end_date;
-		const description_to_add = req.body.description;
-
-		//if parameter is missing while trying to add, return error
-		if (title_to_add == null || employer_name_to_add == null || location_to_add == null || start_date_to_add == null || end_date_to_add == null || description_to_add == null) {
-			console.error("info missing")
-			res.json({ error : "info missing"});			
-		}
-
+		//If no job is found in the job database matching the id, then error.
+		if (response1.rowCount == 0) {
+			res.json({ status: "error: not found"});
+		} 
+		//If the job does exist in the job database, then delete it.
 		else {
-			
-			console.log("job about to be added");
-
-			const template2 = "INSERT INTO jobs (title, employer_name, location, start_date, end_date, description) VALUES ($1, $2, $3, $4, $5, $6)";
-			const response2 = await pool.query(template2, [
-				title_to_add,
-				employer_name_to_add,
-				location_to_add,
-				start_date_to_add,
-				end_date_to_add,
-				description_to_add
-				]);
-			console.log("job added");
-			res.json({ status : "job added" });
-			
+			res.json({ status: "ok", results: response1.rows[0] });
+			console.log(err);
+			const template2 = "DELETE FROM jobs WHERE id = $1";
+			const response1 = await pool.query(template2, id);
+			res.json({status: "listing deleted"});
 		}
-	} 
-
-	catch (err) {
-		console.error(err);
-		res.json({ error : "error adding job" });
+	} catch (err) {
+		res.json({status: "error: listing not deleted"});
+		console.log(err);
 	}
-
 });
 
 app.listen(app.get("port"), () => {
